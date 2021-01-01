63492a118f0f5121ee1b510762b0e3698f6e08c1
Subeen <bueong2kavva2@gmail.com>
2021-01-01 22:33:56 +0900

    알고리즘 스터디

--- 2020.12.28/BJ_1003.cpp
+++ 2020.12.28/BJ_1003.cpp
@@ -2,10 +2,31 @@
 
 using namespace std;
 
-int rpt;       // 반복횟수
-int N;         // N 번째 피보나치 수
-int countOne;  // 1 등장 횟수
-int countZero; // 0 등장 횟수
+int rpt; // 반복횟수
+int N;   // N 번째 피보나치 수
+
+int countZero, countOne = 0; // 0과 1 등장 횟수
+
+void fibonacci(int n)
+{
+    if (n == 0)
+        countZero++;
+    if (n == 1)
+        countOne++;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (n - i == 0)
+            countZero++;
+        if (n - i == 1)
+            countOne++;
+
+        if (n - (i + 1) == 0)
+            countZero++;
+        if (n - (i + 1) == 1)
+            countOne++;
+    }
+}
 
 int main(int argc, char const *argv[])
 {
